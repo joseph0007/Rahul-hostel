12305d63c2d1e38a6595bf7227af75ede46887b0
joseph <josephjoy07@gmail.com>
2020-09-13 17:53:24 +0530

    updated .env file to push to heroku

--- server.js
+++ server.js
@@ -1,28 +1,24 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
-const { data } = require('autoprefixer');
 
-//reading config.env file
+// reading config.env file
 dotenv.config({ path: `${__dirname}/config.env` });
 
-//reading database info from environment variables
-// let database = process.env.DATABASE;
-// const dbpassword = process.env.DATABASE_PASSWORD;
+// reading database info from environment variables
+let database = process.env.DATABASE;
+const dbpassword = process.env.DATABASE_PASSWORD;
 
-// database = database.replace('<PASSWORD>', dbpassword);
+database = database.replace('<PASSWORD>', dbpassword);
 
 //mongoose setup
 mongoose
-  .connect(
-    'mongodb+srv://joseph:g7IoCHkZ0n4tSsgz@cluster0.l0y2z.mongodb.net/natours?retryWrites=true&w=majority',
-    {
-      useNewUrlParser: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-      useUnifiedTopology: true,
-    }
-  )
+  .connect(database, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     console.log('connection successful');
   })
